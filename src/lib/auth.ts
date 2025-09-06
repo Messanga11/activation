1ecf45e20f7bf0bd13eca8cc761b4d73cdeb59f6
__m__ <ndazoomessanga@gmail.com>
2025-09-06 17:05:06 +0100

    fix: uerRole import issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,9 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { organization } from "better-auth/plugins";
-import { UserRole } from "@prisma/client";
 
 import { db } from "./db";
+import { UserRole } from "@/generated/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
